c7585b804856fbb2e1fc64d60a771c41a85fad82
Justin Cypret <jcypret@gmail.com>
2016-11-14 20:55:44 -0600

    Add killport alias

--- aliases.zsh
+++ aliases.zsh
@@ -8,6 +8,10 @@ alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to 
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias myip="ifconfig | grep 'inet ' | grep -v 127.0.0.1 | awk '{print \$2}'"
 
+# Useful when Rails doesn't give the port back.
+# => killport 3000
+killport() { lsof -ti:$1 | xargs kill }
+
 # Project directories
 p() { cd ~/Projects/$1; }
 compctl -W ~/Projects/ -/ p
@@ -19,4 +23,5 @@ desk() { cd ~/Desktop/$1; }
 compctl -W ~/Desktop/ -/ desk
 
 # Misc
-alias emberclean="npm cache clean && bower cache clean && rm -rf node_modules bower_components dist tmp && npm install && bower install"
+alias jsnuke="npm cache clean && bower cache clean && rm -rf node_modules bower_components dist tmp && npm install && bower install"
+alias pgapp="'/Applications/Postgres.app/Contents/Versions/9.5/bin'/psql"
