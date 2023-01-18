0fa928f863738bf33f9e578d32636ce5bd001f16
Justin Cypret <jcypret@gmail.com>
2023-01-17 21:11:53 -0600

    Fix deprecation warning for cmp-nvim-lsp default capabilities

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -44,9 +44,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 end
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- override: default config
 lspconfig.util.default_config =
