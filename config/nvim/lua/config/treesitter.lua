9303a3bf932f8da22ed9f9b88e98e5c7a5b5d84e
Justin Cypret <jcypret@gmail.com>
2023-01-21 23:35:55 -0600

    Enable treesitter indention

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -3,6 +3,9 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     disable = { "markdown" },
   },
+  indent = {
+    enable = true
+  },
   ensure_installed = {
     "bash",
     "c",
