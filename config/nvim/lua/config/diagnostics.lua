eba4ea02febe758505f7812c27c2736301140601
Justin Cypret <jcypret@gmail.com>
2024-02-25 14:51:35 -0600

    Update diagnostic symbols to match trouble

--- config/nvim/lua/config/diagnostics.lua
+++ config/nvim/lua/config/diagnostics.lua
@@ -5,15 +5,14 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
 -- diagnostic symbols
-for _, sign in ipairs({
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-}) do
-  vim.fn.sign_define(sign.name, {
-    texthl = sign.name,
-    text = sign.text,
-    numhl = "",
-  })
+local signs = {
+  Error = "",
+  Warn = "",
+  Hint = "",
+  Info = "",
+}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
