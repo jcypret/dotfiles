461016e85ab8443527277db3a29dc4b1e116edd8
Justin Cypret <jcypret@gmail.com>
2024-02-25 14:16:05 -0600

    Add keymap for toggling trouble

--- config/nvim/lua/plugins/diagnostics.lua
+++ config/nvim/lua/plugins/diagnostics.lua
@@ -2,5 +2,14 @@ return {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = { "Trouble", "TroubleClose", "TroubleToggle" },
+    keys = {
+      {
+        "D",
+        function()
+          require("trouble").toggle("workspace_diagnostics")
+        end,
+      },
+    },
   },
 }
