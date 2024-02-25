3681b7596523bd706d7a23e29b16db06793b8062
Justin Cypret <jcypret@gmail.com>
2024-02-25 15:58:55 -0600

    Hide code folding column

--- config/nvim/lua/plugins/folding.lua
+++ config/nvim/lua/plugins/folding.lua
@@ -7,10 +7,10 @@ return {
     },
     event = "BufEnter",
     init = function()
-      vim.o.foldcolumn = "1"
+      vim.o.foldcolumn = "0"
+      vim.o.foldenable = true
       vim.o.foldlevel = 99
       vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
     end,
     config = true,
     keys = {
