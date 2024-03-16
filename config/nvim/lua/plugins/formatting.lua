2fa0a50ce9db2c51ae22f32c77acda36c9d9a6e4
Justin Cypret <jcypret@gmail.com>
2024-03-16 12:47:30 -0500

    Fix formatting timeouts

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -6,7 +6,9 @@ return {
     event = { "BufWritePre" },
     cmd = { "ConformInfo" },
     opts = {
-      format_on_save = true,
+      format_on_save = {
+        timeout_ms = 5000,
+      },
       formatters_by_ft = {
         ["*"] = { "trim_whitespace" },
         cpp = { "clang_format" },
@@ -28,7 +30,12 @@ return {
       {
         "<leader>f",
         function()
-          require("conform").format()
+          require("conform").format({ timeout_ms = 5000 })
+          vim.notify(
+            "Formatted buffer",
+            vim.log.levels.INFO,
+            { title = "Conform" }
+          )
         end,
         desc = "Format buffer",
       },
