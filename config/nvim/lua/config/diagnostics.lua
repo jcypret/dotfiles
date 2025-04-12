25abcf7240ae6c654cd6e6b0e0854044073fc13d
Justin Cypret <jcypret@gmail.com>
2025-04-12 14:09:14 -0500

    Fix deprecated diagnostic symbols

--- config/nvim/lua/config/diagnostics.lua
+++ config/nvim/lua/config/diagnostics.lua
@@ -10,15 +10,13 @@ vim.diagnostic.config({
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "D", vim.diagnostic.open_float, opts)
 
--- diagnostic symbols
-local signs = {
-  Error = "",
-  Warn = "",
-  Hint = "",
-  Info = "",
-}
-
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "", -- or other icon of your choice here, this is just what my config has:
+      [vim.diagnostic.severity.WARN] = "",
+      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+    },
+  },
+})
